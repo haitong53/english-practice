cb393f84243ca0a8c048110171603ad5dc6ce370
Tong Phuoc Hai <haitong53@gmail.com>
2025-08-24 20:28:20 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getStorage } from "firebase/storage";
-export const storage = getStorage(app);
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
