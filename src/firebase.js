82ea84854bb48ea0780f37ddd5cf60a780d8cac9
Tong Phuoc Hai <haitong53@gmail.com>
2025-08-24 20:11:35 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -21,3 +21,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
